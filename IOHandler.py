bfed3692e5c0ccc451207c4bc269f9f0163596e4
Skyler Hektner <skylerhektner@gmail.com>
2018-07-05 22:00:19 -0600

    MVP working

--- IOHandler.py
+++ IOHandler.py
@@ -13,9 +13,11 @@ class InputState(Enum):
     INCREMENT = 3
 
 class IOHandler:
-    def __init__(self, app):
+    def __init__(self, app, bindingsDict):
         # reference to the gui for raising warning dialogues
         self.app = app
+        # reference to the bindingsDict for triggering actions
+        self.bindingsDict = bindingsDict
 
         # bools for managing thread states
         self.inputActive = False
@@ -25,12 +27,15 @@ class IOHandler:
         # Inventory Snapping Positions
         self.invTopLeftX = 1295
         self.invTopLeftY = 615
-        self.step = 53
+        self.step = 54
         self.currencyCenterX = 335
-        self.currencyCenterY = 520
+        self.currencyCenterY = 540
 
         # bool for tracking shift toggle
         self.shiftOn = False
+
+        # bool for whether or not the player is in mapping mode
+        self.mapping = False
 
         # bools for tracking exit condition (All Four Triggers)
         self.rightClose = False
@@ -87,7 +92,6 @@ class IOHandler:
                         self.inputActive = False
                         self.outputActive = False
                         self.leftClose, self.rightClose, self.leftCloseTrigger, self.rightCloseTrigger = False, False, False, False
-                    
             
             while not self.inputActive:
                 time.sleep(.5)
@@ -97,22 +101,40 @@ class IOHandler:
                 break
     
     def populateInputFunctionMap(self):
-        self.funcMap["ABS_X"] = self.leftAnalogX
-        self.funcMap["ABS_Y"] = self.leftAnalogY
-        self.funcMap["ABS_RX"] = self.rightAnalogX
-        self.funcMap["ABS_RY"] = self.rightAnalogY
-        self.funcMap["ABS_HAT0X"] = self.dPadX
-        self.funcMap["ABS_HAT0Y"] = self.dPadY
-        self.funcMap["ABS_Z"] = self.triggerTwoLeft
-        self.funcMap["ABS_RZ"] = self.triggerTwoRight
-        self.funcMap["BTN_TL"] = self.triggerOneLeft
-        self.funcMap["BTN_TR"] = self.triggerOneRight
-        self.funcMap["BTN_SOUTH"] = self.aButton
-        self.funcMap["BTN_WEST"] = self.xButton
-        self.funcMap["BTN_EAST"] = self.bButton
-        self.funcMap["BTN_NORTH"] = self.yButton
-        self.funcMap["BTN_START"] = self.startButton
-        self.funcMap["BTN_SELECT"] = self.selectButton
+        if (not self.mapping):
+            self.funcMap["ABS_X"] = self.leftAnalogX
+            self.funcMap["ABS_Y"] = self.leftAnalogY
+            self.funcMap["ABS_RX"] = self.rightAnalogX
+            self.funcMap["ABS_RY"] = self.rightAnalogY
+            self.funcMap["ABS_HAT0X"] = self.dPadX
+            self.funcMap["ABS_HAT0Y"] = self.dPadY
+            self.funcMap["ABS_Z"] = self.triggerTwoLeft
+            self.funcMap["ABS_RZ"] = self.triggerTwoRight
+            self.funcMap["BTN_TL"] = self.triggerOneLeft
+            self.funcMap["BTN_TR"] = self.triggerOneRight
+            self.funcMap["BTN_SOUTH"] = self.aButton
+            self.funcMap["BTN_WEST"] = self.xButton
+            self.funcMap["BTN_EAST"] = self.bButton
+            self.funcMap["BTN_NORTH"] = self.yButton
+            self.funcMap["BTN_START"] = self.startButton
+            self.funcMap["BTN_SELECT"] = self.selectButton
+        else:
+            self.funcMap["ABS_X"] = self.leftAnalogX
+            self.funcMap["ABS_Y"] = self.leftAnalogY
+            self.funcMap["ABS_RX"] = self.rightAnalogX
+            self.funcMap["ABS_RY"] = self.rightAnalogY
+            self.funcMap["ABS_HAT0X"] = self.dPadXMapping
+            self.funcMap["ABS_HAT0Y"] = self.dPadYMapping
+            self.funcMap["ABS_Z"] = self.triggerTwoLeftMapping
+            self.funcMap["ABS_RZ"] = self.triggerTwoRightMapping
+            self.funcMap["BTN_TL"] = self.triggerOneLeftMapping
+            self.funcMap["BTN_TR"] = self.triggerOneRightMapping
+            self.funcMap["BTN_SOUTH"] = self.aButton
+            self.funcMap["BTN_WEST"] = self.xButtonMapping
+            self.funcMap["BTN_EAST"] = self.bButton
+            self.funcMap["BTN_NORTH"] = self.yButtonMapping
+            self.funcMap["BTN_START"] = self.startButton
+            self.funcMap["BTN_SELECT"] = self.selectButton
 
     # JOY STICKS
     def leftAnalogY(self, e):
@@ -141,16 +163,34 @@ class IOHandler:
         self.inputState = InputState.INCREMENT
         if (self.inputState == InputState.INCREMENT):
             self.y += self.step * e.state
+    def dPadYMapping(self, e):
+        if (e.state == 0):
+            return
+        elif (e.state == -1):
+            pyautogui.press(self.bindingsDict["Bind Dpad Up"])
+        else:
+            pyautogui.press(self.bindingsDict["Bind Dpad Down"])
     def dPadX(self, e):
         self.inputState = InputState.INCREMENT
         if (self.inputState == InputState.INCREMENT):
             self.x += self.step * e.state
+    def dPadXMapping(self, e):
+        if (e.state == 0):
+            return
+        elif (e.state == -1):
+            pyautogui.press(self.bindingsDict["Bind Dpad Left"])
+        else:
+            pyautogui.press(self.bindingsDict["Bind Dpad Right"])
     
     # TRIGGERS
     def triggerOneLeft(self, e):
         if (e.state):
             pyautogui.press("left")
         self.leftCloseTrigger = bool(e.state)
+    def triggerOneLeftMapping(self, e):
+        if (e.state != 1):
+            return
+        pyautogui.press(self.bindingsDict["Bind Left Button"])
     def triggerTwoLeft(self, e):
         if (e.state == 255):
             self.inputState = InputState.FREE
@@ -159,10 +199,18 @@ class IOHandler:
             self.leftClose = True
         else:
             self.leftClose = False
+    def triggerTwoLeftMapping(self, e):
+        if (e.state != 1):
+            return
+        pyautogui.press(self.bindingsDict["Bind Left Trigger"])
     def triggerOneRight(self, e):
         if (e.state):
             pyautogui.press("right")
         self.rightCloseTrigger = bool(e.state)
+    def triggerOneRightMapping(self, e):
+        if (e.state != 1):
+            return
+        pyautogui.press(self.bindingsDict["Bind Right Button"])
     def triggerTwoRight(self, e):
         if (e.state == 255):
             self.rightClose = True
@@ -171,15 +219,20 @@ class IOHandler:
             self.y = self.invTopLeftY
         else:
             self.rightClose = False
+    def triggerTwoRightMapping(self, e):
+        if (e.state != 1):
+            return
+        pyautogui.press(self.bindingsDict["Bind Right Trigger"])
     
     # BUTTONS
     def aButton(self, e):
-        if (e.state != 1):
-            return
+        if (e.state == 1):
+            pyautogui.mouseDown()
+        elif (e.state == 0):
+            pyautogui.mouseUp()
         if (self.shiftOn):
             pyautogui.keyUp("shiftleft")
             self.shiftOn = False
-        pyautogui.click()
     def yButton(self, e):
         if (e.state != 1):
             return
@@ -187,7 +240,10 @@ class IOHandler:
             pyautogui.keyDown("shiftleft")
             self.shiftOn = True
         pyautogui.click()
-        
+    def yButtonMapping(self, e):
+        if (e.state != 1):
+            return
+        pyautogui.press(self.bindingsDict["Bind Y Button"])
     def bButton(self, e):
         if (e.state != 1):
             return
@@ -204,12 +260,19 @@ class IOHandler:
         pyautogui.keyDown("ctrlleft")
         pyautogui.click()
         pyautogui.keyUp("ctrlleft")
+    def xButtonMapping(self, e):
+        if (e.state != 1):
+            return
+        pyautogui.press(self.bindingsDict["Bind X Button"])
     
     #START/SELECT
     def startButton(self, e):
         if (self.shiftOn):
             pyautogui.keyUp("shiftleft")
             self.shiftOn = False
+        if (e.state):
+            self.mapping = not self.mapping
+            self.populateInputFunctionMap()
     def selectButton(self, e):
         if (e.state):
             pyautogui.press("escape")
@@ -218,6 +281,7 @@ class IOHandler:
             self.shiftOn = False
     
     def outputLoop(self):
+        lastX, lastY = 0, 0
         while 1:
             while self.outputActive:
                 if (self.inputState == InputState.FREE):
--- main.py
+++ main.py
@@ -1,14 +1,61 @@
 from appJar import gui
 from IOHandler import IOHandler, InputState
+import pyautogui
+import inputs
+import re
 
 # MAIN APPLICATION
 class PoXe:
     def __init__(self):
         self.app = gui()
-        self.ioHandler = IOHandler(self.app)
-        self.app.addButton("START", self.startTracking)
+        self.bindingsDict = dict()
+        self.configureBindingsDict()
+        self.configureUI()
+        self.ioHandler = IOHandler(self.app, self.bindingsDict)
         self.app.setStopFunction(self.closeThreads)
         self.app.go()
+
+    def configureBindingsDict(self):
+        self.bindingsDict["Bind X Button"] = "r"
+        self.bindingsDict["Bind Y Button"] = "f"
+        self.bindingsDict["Bind Left Button"] = "2"
+        self.bindingsDict["Bind Right Button"] = "3"
+        self.bindingsDict["Bind Left Trigger"] = "1"
+        self.bindingsDict["Bind Right Trigger"] = "4"
+        self.bindingsDict["Bind Dpad Down"] = "w"
+        self.bindingsDict["Bind Dpad Up"] = "t"
+        self.bindingsDict["Bind Dpad Left"] = "q"
+        self.bindingsDict["Bind Dpad Right"] = "e"
+
+    def configureUI(self):
+        # NAME
+        self.app.title = "PoXe"
+
+        # START BUTTON
+        self.app.addButton("START", self.startTracking, 0, 0, 2, 2)
+
+        # CONTROLS CONFIGURATIONS
+        self.app.addLabel("A_Label", "A Binding: Left Click", 2, 0)
+        self.app.setLabelAlign("A_Label", "left")
+        self.app.addLabel("B_Label", "B Binding: Right Click", 3, 0)
+        self.app.setLabelAlign("B_Label", "left")
+
+        self.createBindingSet("Bind X Button", "X Binding: "+ self.bindingsDict["Bind X Button"], 4, 0)
+        self.createBindingSet("Bind Y Button", "Y Binding: " + self.bindingsDict["Bind Y Button"], 5, 0)
+        self.createBindingSet("Bind Left Button", "LB Binding: " + self.bindingsDict["Bind Left Button"], 6, 0)
+        self.createBindingSet("Bind Right Button", "RB Binding: " + self.bindingsDict["Bind Right Button"], 7, 0)
+        self.createBindingSet("Bind Left Trigger", "LT Binding: " + self.bindingsDict["Bind Left Trigger"], 8, 0)
+        self.createBindingSet("Bind Right Trigger", "RT Binding: " + self.bindingsDict["Bind Right Trigger"], 9, 0)
+        self.createBindingSet("Bind Dpad Down", "DPad Down Binding: " + self.bindingsDict["Bind Dpad Down"], 10, 0)
+        self.createBindingSet("Bind Dpad Up", "DPad Up Binding: " + self.bindingsDict["Bind Dpad Up"], 11, 0)
+        self.createBindingSet("Bind Dpad Left", "DPad Left Binding: " + self.bindingsDict["Bind Dpad Left"], 12, 0)
+        self.createBindingSet("Bind Dpad Right", "DPad Right Binding: " + self.bindingsDict["Bind Dpad Right"], 13, 0)
+
+    def createBindingSet(self, label, labelText, row, column):
+        self.app.addLabel(label + "L", labelText, row, column)
+        self.app.setLabelAlign(label + "L", "left")
+        self.app.addButton(label, self.bindNewKey, row, column + 1)
+        self.app.setButtonAlign(label, "left")
     
     def startTracking(self):
         self.ioHandler.inputActive = True
@@ -19,6 +66,21 @@ class PoXe:
         self.ioHandler.inputActive = False
         self.ioHandler.outputActive = False
         return True
+
+    def bindNewKey(self, btn):
+        result = self.app.stringBox("Key Binding", btn + ": Please only enter a single letter or number (0-9)")
+        match = re.search("[A-Z,a-z,0-9]", result)
+        try:
+            self.bindingsDict[btn] = match.group()
+            self.app.setLabel(btn + "L", self.replaceLastChar(self.app.getLabel(btn + "L"), match.group()))
+        except:
+            self.app.warningBox("Invalid Entry", "Sorry, your entry was invalid. Please enter a single letter or number(0-9)")
+
+    def replaceLastChar(self, oldString, newLastChar):
+        s = oldString[:-1]
+        s += newLastChar
+        return s
+        
 
 if __name__ == "__main__":
     app = PoXe()
